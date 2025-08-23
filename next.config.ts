f999dbc13e875466c48d3e31e554ced1bdc2378f
curenosuke <Yamanouchi.Yuya@gmail.com>
2025-08-23 17:08:54 +0900

    デプロイのためcongigjsを修正

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,29 @@
-import type { NextConfig } from "next";
+// ローカルの場合はこちら
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
-      },
-    ];
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'http://localhost:8000/api/:path*',
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+
+// デプロイする場合はこちら
+require('dotenv').config()
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  env: {
+    // Reference a variable that was defined in the .env file and make it available at Build Time
+    API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
   },
-};
-
-export default nextConfig;
+}
+module.exports = nextConfig
